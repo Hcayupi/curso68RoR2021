49aba3b0fed4f2396a91300b3aae23f632b9f360
Hcayupi <hipolito.cayupi@gmail.com>
2021-05-28 19:44:21 -0400

    Actividad Test bootstrap terminado

---
+++ Actividades DOJO/JS/3.- Algoritmo IV/Ejercicios.js
@@ -0,0 +1,66 @@
+/*1.- Dados un array y un valor Y, cuenta e imprime (print) el número de valores del array que sean mayores que Y.  */
+
+function arrayMayorQueY(array, y) {
+    var contador = 0;
+
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] > y) {
+            console.log(array[i] + " es mayor que " + y);
+            contador += 1;
+        }
+    }
+    console.log(contador + " números son mayores que " + y);
+}
+
+arrayMayorQueY([2, 3, 58, -5, 878, 52, 1, 2, 3, 8, 5], 12);
+
+
+
+/*2.- Dado un array, imprime los valores máximos (max), mínimos (min) y promedio (average) para el array.  */
+
+function mayorMenorProm(array) {
+    var mayor = array[0];
+    var menor = 0;
+    var suma = 0;
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] > mayor) {
+            mayor = array[i];
+        } else if (array[i] < menor) {
+            menor = array[i];
+        }
+        suma += array[i];
+    }
+    var promedio = suma / array.length;
+    console.log("Mayor: " + mayor);
+    console.log("Menor: " + menor)
+    console.log("Promedio: " + promedio);
+}
+
+mayorMenorProm([2, 3, 58, -5, 878, 52, 1, 2, -10, 0, 1000]);
+
+/*3.- Dado un array de números, crea una función que dé como resultado un nuevo array donde los valores negativos se 
+reemplacen por el texto (string) ‘Dojo’. Por ejemplo, reemplazarNegativos([1,2,-3,-5,5]) debiera devolver [1,2, “Dojo”, “Dojo”, 5]. */
+
+function reemplazarNegativos(array) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] < 0) {
+            array[i] = "Dojo";
+        }
+    }
+    console.log("Nuevo array: [" + array + "]");
+}
+reemplazarNegativos([1, 2, -3, -5, 5]);
+
+/*4.- Dado un array y su respectivo índice, remueve los valores en el rango del índice 
+dado( acortando el array). Por ejemplo, removerRango([20,30,40,50,60,70],2,4)debiera devolver [20,30,70].*/
+
+function removerRango(array, desde, hasta) {
+    var nuevoArray = [];
+    for (var i = 0; i < array.length; i++) {
+        if (i < desde || i > hasta) {
+            nuevoArray.push(array[i]);
+        }
+    }
+    console.log("Nuevo array: [" + nuevoArray + "]");
+}
+removerRango([20, 30, 40, 50, 60, 70], 2, 4);
