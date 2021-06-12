707f988e7b129f3b10d09662f3a978638d7eedf9
Hcayupi <hipolito.cayupi@gmail.com>
2021-06-11 20:33:16 -0400

    Avance de pacman

--- Actividades_DOJO/JS/4.- Algoritmo V/AlgoritmoV.js
+++ Actividades_DOJO/JS/4.- Algoritmo V/AlgoritmoV.js
@@ -25,14 +25,13 @@ function moverAdelante(array) {
 
 console.log(moverAdelante([1, 2, 3, 34, 4, 5, 6, 6, 7]));
 
-/*3.-Configura un array para que el resultado sean los valores en el orden contrario. Por ejemplo, returnReverso([1,2,3]) debe dar [3,2,1]. */
+/*3.-Configura un array para que el resultado sean los valores en el orden contrario. 
+Por ejemplo, returnReverso([1,2,3]) debe dar [3,2,1]. */
 
 function returnReverso(array) {
     var arrayAux = [];
-    var contador = 0;
     for (var i = array.length - 1; i >= 0; i--) {
-        arrayAux[contador] = array[i];
-        contador++;
+        arrayAux.push(array[i]);
     }
 
     return arrayAux;
@@ -41,7 +40,8 @@ function returnReverso(array) {
 console.log(returnReverso([1, 2, 3]));
 
 /**4.- Crea una función que cambie un array repitiendo sus valores originales (manteniendo el mismo orden). 
- * Por ejemplo, repetirValores([4,”Ulysses”, 42, false]) debiera dar [4,4, “Ulysses”, “Ulysses”, 42, 42, false, false]. */
+ * Por ejemplo, repetirValores([4,”Ulysses”, 42, false]) debiera dar [4,4, “Ulysses”, “Ulysses”, 42, 42, 
+ * false, false]. */
 
 function repetirValores(array) {
     var arrayAux = [];
--- Actividades_DOJO/JS/7.- Fundamentos_básicosII/ejercicio_en_equipo.js
+++ Actividades_DOJO/JS/7.- Fundamentos_básicosII/ejercicio_en_equipo.js
@@ -51,12 +51,41 @@ function cambiaHaciaElCentro(array) {
         array[iFinal] = auxiliar;
         iFinal -= 2;
     }
-
     return array;
 }
 
 console.log(" salida final: " + cambiaHaciaElCentro([1, 2, 3, 4, 5, 6, 45, 23, 58, 9, 54, 25, 852, 458, 42, 12, 36, 98]));
 console.log(" salida final: " + cambiaHaciaElCentro([true, 42, "Ada", 2, "pizza"]));
+
+/*Ejercicio 13.*/
+/*Cambiar hacia el centro -  Dado un array, cambia el primer y último valor, el tercero con el ante penúltimo, etc.
+Ejemplo: cambiaHaciaElCentro([true, 42, “Ada”, 2, “pizza”]) cambia el array a [“pizza¨, 42, “Ada”, 2, true]. 
+cambiaHaciaElCentro([1,2,3,4,5,6]) cambia el array a [6,2,4,3,5,1]. No es necesario devolver (return) el array esta vez.*/
+
+function cambiaHaciaElCentro(array) {
+    var primer = [];
+    var tercero = [];
+    primer = array[array.length - 1];
+    array[array.length - 1] = array[0];
+    array[0] = primer;
+    tercero = array[array.length / 2];
+    array[array.length / 2] = array[(array.length / 2) - 1];
+    array[(array.length / 2) - 1] = tercero;
+
+    return array
+}
+console.log(cambiaHaciaElCentro([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+
+//opcion 2//
+function cambiaHaciaElCentro(array) {
+    for (var i = 0; i < array.length / 2; i += 2) {
+        var temp = array[i];
+        array[i] = array[array.length - 1 - i];
+        array[array.length - 1 - i] = temp;
+    }
+    return array;
+}
+console.log(cambiaHaciaElCentro([1, 2, 3, 4, 5, 6])); //cambia el array a [6,2,4,3,5,1
 
 /*14.- Escala el Array - Dado un array arr y un número num, multiplica todos los valores en el array arr por el número num, 
 y devuelve el array arr modificado. Por ejemplo, escalaArray([1,2,3], 3] debería devolver [3,6,9].*/
