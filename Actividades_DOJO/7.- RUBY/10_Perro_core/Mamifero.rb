3e392758e41e2b0f643151417d679ec00750408b
Hcayupi <hipolito.cayupi@gmail.com>
2021-07-23 23:45:57 -0400

    Subida del Perro

---
+++ Actividades_DOJO/7.- RUBY/10_Perro_core/Mamifero.rb
@@ -0,0 +1,14 @@
+#@Hcayupi
+
+class Mamifero
+  attr_accessor :salud
+
+  def initialize (salud=150)
+     @salud = salud
+  end
+
+  def mostrar_salud
+    puts @salud
+  end
+end
+
--- Actividades_DOJO/7.- RUBY/9_mi_enumerable.rb
+++ Actividades_DOJO/7.- RUBY/9_mi_enumerable.rb
@@ -11,5 +11,5 @@ class Array
    include MiEnumerable
 end
 
-#[1,2,3,4].mi_each { |i| puts i } # => 1 2 3 4
+[1,2,3,4].mi_each { |i| puts i } # => 1 2 3 4
 [1,2,3,4].mi_each { |i| puts i * 10 } # => 10 20 30 40
